40b5ccf075bf007ec59ad72e6c016fc1a5c74ffd
Miguetheking533 <miguelito050124@gmail.com>
2025-10-01 14:38:00 -0600

    se subieron archivos

---
+++ dibujow.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(600, 400);
+}
+function draw() {
+  background(135, 206, 235);
+  fill("yellow");
+  stroke("orange"); 
+  strokeWeight(20); 
+  circle(550, 50, 100);
+  stroke(0);
+  strokeWeight(1)
+  fill("green");
+  rect(0, 200, 600, 200);
+
+  textSize(40)
+  text("🐒", 400, 214) 
+   textSize(20)
+  text("🥥", 300, 200) 
+  textSize(125)
+  text("🌴", 300, 200) 
+  text("🌴", 100, 200) 
+
+  textSize(25)
+ text("🏝️", 300, 70) 
+
+ textSize(50)
+ text("🚤", 20, 150) 
+
+  
+ textSize(70)
+ text("🧘🏽‍♂️", 200, 300) 
+
+ 
+ textSize(100)
+ text("☁️", 40, 80) 
+
+ textSize(100)
+ text("🐎", 40, 300) 
+ 
+}
